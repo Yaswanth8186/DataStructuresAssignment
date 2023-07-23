2e057837dad54e6d6a9d779624cf4f76520874ac
Yaswanth Kumar Reddy Bussa <130962487+Yaswanth8186@users.noreply.github.com>
2023-07-23 20:15:33 +0530

    Created CumulativeMultiple.java

---
+++ CumulativeMultiple.java
@@ -0,0 +1,30 @@
+//Question 1: Cumulative Multiple of an Array with exception handling
+//Sample input: arrNum = [5, 3, 4, 2, 0, 8]
+//Output:- arrNum = [5, 15, 60, 120, 0, 0]
+//java program
+import java.util.*;
+public class CumulativeMultiple {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int arraysize;
+		int[] array;
+		try {
+			System.out.print("Enter the array size : ");
+			arraysize = sc.nextInt();
+			array = new int[arraysize];
+			System.out.print("Enter the array elements : ");
+			for(int i = 0;i < arraysize; i++)
+				array[i] = sc.nextInt();
+			for(int i = 1; i < arraysize;i++)
+				array[i] = array[i - 1] * array[i];
+			System.out.print("arrNum = [");
+			for(int i = 0;i < arraysize; i++)
+				if(i != arraysize - 1)
+				 System.out.print(array[i] + ",");
+				else
+					System.out.print(array[i] + "]");
+		}catch(Exception e) {
+				System.out.println("Enter valid input");
+		}
+	}
+}
